d923e0039efe5aeba5f2c74732cd60f40de42b8a
bauen1 <jh@JH-MBA.local>
2016-03-26 13:04:14 +0100

    First try at parsing responses

--- core/utils.lua
+++ core/utils.lua
@@ -33,6 +33,33 @@ function utils.list (directory) -- Hacky pice of sh**
   end
 end
 
+function utils.parse (line)
+  -- Example:
+  -- :prefix command arg1 arg2 ...
+  local args,arg = {},""
+  local prefix, other = string.match (line, "^:([^%s]*) (.*)$")
+
+  if not other then -- if theres no prefix, then other will be nil
+    other = line
+  end
+
+  local cmd, other = string.match (other, "^([^%s]*)(.*)$")
+
+  -- other = "arg1 arg2 arg3 ..."
+
+  for i=0,100 do -- FIXME: This is broken
+    arg, other = string.match (other, "^([^%s]*)(.*)$")
+    if arg and (arg~="") then
+      table.insert (args, arg)
+    end
+
+    if not arg or not other or other=="" or arg=="" then break end
+  end
+
+
+  return prefix, cmd, table.pack (table.unpack(args))
+end
+
 function utils.printf (...)
   return print (string.format (...))
 end
